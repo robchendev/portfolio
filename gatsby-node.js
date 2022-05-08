0b52d422ef7b1906534a2df514a8ac67e4e1bfd6
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-05-08 15:24:21 -0700

    initial commit from vikelabs repo

---
+++ gatsby-node.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+
+  const { data } = await graphql(`
+    query ProjectBlog {
+      projects: allMarkdownRemark(
+        filter: {frontmatter: {type: {eq: "project"}}}
+      ) {
+        nodes {
+          frontmatter {
+            title
+          }
+        }
+      }
+      blogs: allMarkdownRemark(
+        filter: {frontmatter: {type: {eq: "blog"}}}
+      ) {
+        nodes {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  `)
+
+  data.projects.nodes.forEach(node => {
+    actions.createPage({
+      path: '/p/' + node.frontmatter.title.toLowerCase().replace(/\ /g, '-'),
+      component: path.resolve('./src/templates/project/project.js'),
+      context: {
+        title: node.frontmatter.title,
+      },
+    })
+  })
+
+  data.blogs.nodes.forEach(node => {
+    actions.createPage({
+      path: '/b/' + node.frontmatter.title.toLowerCase().replace(/\ /g, '-'),
+      component: path.resolve('./src/templates/blog/blog.js'),
+      context: {
+        title: node.frontmatter.title,
+      },
+    })
+  })
+}
